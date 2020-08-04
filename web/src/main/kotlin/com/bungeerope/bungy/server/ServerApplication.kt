de825509afcd4984617caacd1ce17b8964d26705
ZhihaoQuinn <howzequinn@gmail.com>
2020-08-04 14:18:55 +0800

    build(project): Init project
    Build restful api based on Spring-WebFlux.

---
+++ web/src/main/kotlin/com/bungeerope/bungy/server/ServerApplication.kt
@@ -0,0 +1,11 @@
+package com.bungeerope.bungy.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class ServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerApplication>(*args)
+}
