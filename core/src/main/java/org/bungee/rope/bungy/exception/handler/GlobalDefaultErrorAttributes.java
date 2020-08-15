bd9cecdd62adef74c0ed0596caa1f957acb16e3f
ZhihaoQuinn <howzequinn@gmail.com>
2020-08-15 23:19:04 +0800

    refactor(project): perfect project structure
    Perfect project structure.

--- core/src/main/java/org/bungee/rope/bungy/exception/handler/GlobalDefaultErrorAttributes.java
+++ core/src/main/java/org/bungee/rope/bungy/exception/handler/GlobalDefaultErrorAttributes.java
@@ -15,24 +15,24 @@ import java.util.Map;
 @Component
 public class GlobalDefaultErrorAttributes extends DefaultErrorAttributes {
 
-	/*
-	 * 重写父类默认错误属性，如果是BaseException将重新封装错误属性值，否则返回默认值
-	 */
-	@Override
-	public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-		Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
-		if (getError(webRequest) instanceof BaseException) {
-			BaseException be = (BaseException) getError(webRequest);
-			errorAttributes.put("exception", be.getClass().getSimpleName());
-			errorAttributes.put("message", be.getMessage());
-			errorAttributes.put("status", be.getStatus());
-			errorAttributes.put("error", be.getStatus().getReasonPhrase());
-			return errorAttributes;
-		}
-		errorAttributes.put("exception", "SystemException");
-		errorAttributes.put("message", "System Error , Check logs!");
-		errorAttributes.put("status", "500");
-		errorAttributes.put("error", " System Error ");
-		return errorAttributes;
-	}
+    /*
+     * 重写父类默认错误属性，如果是BaseException将重新封装错误属性值，否则返回默认值
+     */
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+        if (getError(webRequest) instanceof BaseException) {
+            BaseException be = (BaseException) getError(webRequest);
+            errorAttributes.put("exception", be.getClass().getSimpleName());
+            errorAttributes.put("message", be.getMessage());
+            errorAttributes.put("status", be.getStatus());
+            errorAttributes.put("error", be.getStatus().getReasonPhrase());
+            return errorAttributes;
+        }
+        errorAttributes.put("exception", "SystemException");
+        errorAttributes.put("message", "System Error , Check logs!");
+        errorAttributes.put("status", "500");
+        errorAttributes.put("error", " System Error ");
+        return errorAttributes;
+    }
 }
--- core/src/main/java/org/bungee/rope/bungy/exception/handler/SysExceptionHandler.java
+++ core/src/main/java/org/bungee/rope/bungy/exception/handler/SysExceptionHandler.java
@@ -25,30 +25,30 @@ import java.util.Map;
 @Order(-2) // 标记AspectJ的切面程序，值越小拥有越高的优先级，这里设置较高的优先级。
 public class SysExceptionHandler extends AbstractErrorWebExceptionHandler { // AbstractErrorWebExceptionHandler，此抽象类用来拓展与实现全局错误处理
 
-	/*
-	 * 构造函数将GlobalDefaultErrorAttributes全局异常属性实现类的值设置到AbstractErrorWebExceptionHandler抽象类的局部变量中去
-	 */
-	public SysExceptionHandler(GlobalDefaultErrorAttributes errorAttributes, ApplicationContext applicationContext,
-			ServerCodecConfigurer serverCodecConfigurer) {
-		super((ErrorAttributes) errorAttributes, new ResourceProperties(), applicationContext);
-		super.setMessageWriters(serverCodecConfigurer.getWriters());
-		super.setMessageReaders(serverCodecConfigurer.getReaders());
-	}
+    /*
+     * 构造函数将GlobalDefaultErrorAttributes全局异常属性实现类的值设置到AbstractErrorWebExceptionHandler抽象类的局部变量中去
+     */
+    public SysExceptionHandler(GlobalDefaultErrorAttributes errorAttributes, ApplicationContext applicationContext,
+                               ServerCodecConfigurer serverCodecConfigurer) {
+        super((ErrorAttributes) errorAttributes, new ResourceProperties(), applicationContext);
+        super.setMessageWriters(serverCodecConfigurer.getWriters());
+        super.setMessageReaders(serverCodecConfigurer.getReaders());
+    }
 
-	/*
-	 * 重写getRoutingFunction方法，设置对应的RequestPredicates、Mono服务响应对象
-	 */
-	@Override
-	protected RouterFunction<ServerResponse> getRoutingFunction(final ErrorAttributes errorAttributes) {
-		return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
-	}
+    /*
+     * 重写getRoutingFunction方法，设置对应的RequestPredicates、Mono服务响应对象
+     */
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(final ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
 
-	/*
-	 * 将全局异常属性值设置到新的ServerResponse中
-	 */
-	private Mono<ServerResponse> renderErrorResponse(final ServerRequest request) {
-		final Map<String, Object> errorPropertiesMap = getErrorAttributes(request, false);
-		return ServerResponse.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON_UTF8)
-				.body(BodyInserters.fromValue(errorPropertiesMap));
-	}
+    /*
+     * 将全局异常属性值设置到新的ServerResponse中
+     */
+    private Mono<ServerResponse> renderErrorResponse(final ServerRequest request) {
+        final Map<String, Object> errorPropertiesMap = getErrorAttributes(request, false);
+        return ServerResponse.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(BodyInserters.fromValue(errorPropertiesMap));
+    }
 }
